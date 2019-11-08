4e0abce7e1b03a9606fc80cab56acc605e994d50
kyosuke uryu <kyo.uryu33@gmail.com>
2019-11-08 11:55:04 +0900

    usersコントローラー、トップページ仮作成

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,4 @@
+class UsersController < ApplicationController
+  def tops
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'users#tops'
+
+  resources :users, except: [:index] do
+    collection do
+      get :tops
+    end
+  end
 end
