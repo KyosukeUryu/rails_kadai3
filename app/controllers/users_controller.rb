cf876507addc8fcb71d447dccab21351726f3192
kyosuke uryu <kyo.uryu33@gmail.com>
2019-11-08 12:16:32 +0900

    ユーザ新規登録機能仮実装,バリデーション導入

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,23 @@
 class UsersController < ApplicationController
   def tops
   end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to root_path, success: 'ユーザ登録完了'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation, :image)
+  end
 end
